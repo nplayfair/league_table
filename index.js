ddcf018a1307fbdca279ef6b14372e14d56f55b0
Nick Playfair <nick@playfair.dev>
2021-08-12 23:50:47 +0100

    Validate params on v2 api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const redis = require('redis');
 const cors = require('cors');
+const { param, validationResult } = require('express-validator');
 const app = express();
 
 const redisConfig = {
@@ -19,7 +20,7 @@ client.on('error', (err) => {
 app.use(cors());
 
 function LeagueSeason(leagueID, year) {
-  this.params = {league: leagueID, season: year}
+  this.params = { league: leagueID, season: year }
   this.headers = {
     'x-rapidapi-host': process.env.API_HOST,
     'x-rapidapi-key': process.env.API_KEY,
@@ -98,39 +99,52 @@ app.get('/championship', async (req, res) => {
   }
 });
 
-app.get('/v2/:league/:year', async (req, res) => {
-  const league = req.params.league;
-  const year = req.params.year;
-  const query = league + year;
-  try {
-    client.get(query, async (err, leagueTable) => {
-      if (err) throw err;
+app.get('/v2/:league/:year', 
+  [
+    // League id must be one of these
+    param('league').isIn(['39', '40', '41', '42']),
+    // Year must be no earlier than 2018
+    param('year').isIn(['2018', '2019', '2020', '2021']),
+  ],
+  async (req, res) => {
+    // Validate
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    // Begin method
+    const league = req.params.league;
+    const year = req.params.year;
+    const query = league + year;
+    try {
+      client.get(query, async (err, leagueTable) => {
+        if (err) throw err;
 
-      // Return cached league table if present
-      if (leagueTable) {
-        res.status(200).send({
-          table: JSON.parse(leagueTable),
-          message: 'data retrieved from cache',
-        });
-      } else {
-        // Fetch from the API
-        reqSeason = new LeagueSeason(league, year);
-        const leagueTable = await axios.get(
-          'https://v3.football.api-sports.io/standings',
-          reqSeason
-        );
-        // Save result to cache
-        client.setex(query, 43200, JSON.stringify(leagueTable.data));
-        // Return data from API
-        res.status(200).send({
-          table: leagueTable.data,
-          message: 'cache miss',
-        });
-      }
-    });
-  } catch (err) {
-    res.status(500).send({ message: err.message });
-  }
+        // Return cached league table if present
+        if (leagueTable) {
+          res.status(200).send({
+            table: JSON.parse(leagueTable),
+            message: 'data retrieved from cache',
+          });
+        } else {
+          // Fetch from the API
+          reqSeason = new LeagueSeason(league, year);
+          const leagueTable = await axios.get(
+            'https://v3.football.api-sports.io/standings',
+            reqSeason
+          );
+          // Save result to cache
+          client.setex(query, 43200, JSON.stringify(leagueTable.data));
+          // Return data from API
+          res.status(200).send({
+            table: leagueTable.data,
+            message: 'cache miss',
+          });
+        }
+      });
+    } catch (err) {
+      res.status(500).send({ message: err.message });
+    }
 });
 
 app.listen(process.env.PORT || 3001, () => {
